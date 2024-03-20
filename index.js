cba5c4caeef829fd94afba16e480e9446248e394
ashesbloom <mayankpandeydk123@gmail.com>
2024-03-21 03:56:49 +0530

    added creating user funtionality in post request

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const port = 8000;
+const fs = require('fs');
 const express = require('express');
 const app = express();
-// we will use a website called https://mockaroo.com/ to get the scrap data
+// using a website called https://mockaroo.com/ to get the scrap data
 const data = require('./MOCK_DATA.json');
+
+//middleware - plugin
+app.use(express.urlencoded({extended:false})) //this middleware will convert the data sent by the client into json object
 
 app.get('/api/user',(req,res) => {
     return res.json(data);
@@ -14,19 +18,31 @@ app.get('/user',(req,res)=>{
     </ul>`
     return res.send(html);
 })
+
+//using postman api to testout client request
+
+app.post('/api/user',(req,res)=>{
+    //creating user
+    const body = req.body; //data sent by the client get is stored in body
+    console.log(body); //printing the data sent by the client 
+    //but to understand the data we have to use middleware called urlencoded to change the data into json object
+    data.push({id: data.length + 1,...body});  //pushing the body with the previous data
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(data),(err,pass)=>{ //rewritting new body pushed file with the old one 
+        //avoid using appendfile to eliminate any duplicate entries
+        return res.json(`Your data have be added id:${data.length}`);
+    });
+    
+})
+
 /*
  to get the user by id we have to create Dynamic path parameter
  GET /user/:id (we can achive that by using make dynamic variable {:id})
  */
-app.route('/user/:id') //as here pathname for GET,POST,PATCH,DELETE is same so we group the pathname together using route
+app.route('/user/:id') //as, here pathname for GET,PATCH,DELETE is same so we will group the pathname together using route
     .get((req,res)=>{
-        const id = Number(req.params.id); // here we will call the parameter id(pathname request by a client) and convert it to a number 
-        const user = data.find((user_id)=>user_id.id===id); // here we will find the user by id
+        const id = Number(req.params.id); // calling the parameter id(pathname request by a client) and convert it to a number 
+        const user = data.find((user_id)=>user_id.id===id); // finding the user by id
         return res.json(user);
-    })
-    .post((req,res)=>{
-        //create user
-        return res.json({status:'Pending...'});
     })
     .patch((req,res)=>{
         //edit user
